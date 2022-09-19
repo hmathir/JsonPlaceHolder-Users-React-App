62cbcd9b858dd52391196302ef43c3608225ad54
hmathir <hmathir@gmail.com>
2022-09-20 03:21:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FetchUsers></FetchUsers>
     </div>
   );
+}
+
+function FetchUsers () {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+     const fetchMyAPI = async() => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await res.json();
+      setUsers(data)
+    }
+    fetchMyAPI();
+  }, [])
+  return (
+    <div>
+    {
+      users.map(user => <Users name={user.name} email={user.email} address={user.address}></Users>)
+    }
+    </div>
+  )
+}
+
+function Users (props){
+  const {name,email} = props;
+  const {zipcode, city, street} = props.address;
+  return (
+    <div className='users-container'>
+      <h1>{name}</h1>
+      <p>{email}</p>
+      <div>
+        <CityCom city={city} zipcode={zipcode} street={street}></CityCom>
+      </div>
+    </div>
+  )
+}
+
+function CityCom (props) {
+  const {city, street, zipcode} = props;
+  return (
+    <div>
+      <p>{street}, {zipcode}, {city}.</p>
+    </div>
+  )
 }
 
 export default App;
